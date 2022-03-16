131b0b8739091e37538d51d69c751dd0740cb204
najwanan <najwanan@hotmail.com>
2022-03-15 23:19:15 -0700

    added weather component to pull from API

---
+++ src/components/GetWeather.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{data}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App", 
+    data(){
+        return {
+            data:{}
+        }
+    },
+
+    beforeMount(){
+        await this.getForecast();
+    },
+    methods: {
+        async getForecast(){
+            const response = await fetch(http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key});
+            const data = await.response.json();
+            this.data = data;
+        }
+    }
+}
+</script>

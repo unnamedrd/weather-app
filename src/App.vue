ac7a947209265d4d097128f335a3d9167484a19c
najwanan <najwanan@hotmail.com>
2022-03-15 22:57:12 -0700

    initial set up of file

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
 
+<template>
+<HelloWorld/>
+</template>
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 

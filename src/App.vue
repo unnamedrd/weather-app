3fe888c20e06bab6c73bfb1fc5a846414cd741ac
najwanan <theunnamedrd@gmail.com>
2022-03-18 21:58:45 -0700

    added card component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <v-app>
     <v-main>
       <HelloWorld/>
+      <WeatherCard/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import WeatherCard from './components/WeatherCards'
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    WeatherCard
   },
 
   data: () => ({
---
+++ src/components/WeatherCards.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card
+    class="mx-auto"
+    max-width="344"
+    >
+    <v-card-header-text>
+      <v-card-title>Monday</v-card-title>
+
+      <v-card-subtitle>Forecast</v-card-subtitle>
+    </v-card-header-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+   name: 'WeatherCard',
+   data: ()=>({
+       show: false,
+   }),
+}
+</script>
